d82095d0db2cc4f48ef2ba0115f6be5ee0e64ab1
MN08 <wakhid.wijaya@students.amikom.ac.id>
2019-07-01 22:27:22 +0700

    done fucker

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,10 +2,22 @@
 var checker = document.getElementById('gridCheck');
 var sendbtn = document.getElementById('btnSubmit');
 // when unchecked or checked, run the function
-checker.onchange = function() {
+checker.onchange = function () {
 	if (this.checked) {
 		sendbtn.disabled = false;
 	} else {
 		sendbtn.disabled = true;
 	}
 };
+
+
+//seaarch
+
+$(document).ready(function () {
+	$("#myInput").on("keyup", function () {
+		var value = $(this).val().toLowerCase();
+		$("#myTable tr").filter(function () {
+			$(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+		});
+	});
+});
